cc1aee5debd6aef8a5de9599c2ae303655440add
Firli Ilhami <firli.ilhami@gmail.com>
2023-03-16 13:39:07 +0700

    decoupled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// arguments
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// logger
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,8 +24,9 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 
 	log.Fatal(err)
 }
