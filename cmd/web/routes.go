83ca48e73b31c407fc4f923f00bea4dbcc141a61
Firli Ilhami <firli.ilhami@gmail.com>
2023-04-27 04:04:25 +0700

    change authenticate

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,7 +22,7 @@ func (app *application) routes() http.Handler {
 
 	// a new middleware chain containing the middleware specific to
 	// our dynamic application routes
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	// unprotected routes
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
